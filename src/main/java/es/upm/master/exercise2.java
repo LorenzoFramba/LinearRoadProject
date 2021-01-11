1c87087484a6d92f6bce1c1043b5462a7c1203f7
LorenzoFramba <lorenzoframba-1@studenti.unitn.it>
2021-01-11 15:34:27 +0100

    secondo commit

--- src/main/java/es/upm/master/exercise2.java
+++ src/main/java/es/upm/master/exercise2.java
@@ -107,18 +107,18 @@ public class exercise2 {
                 ).keyBy(3);
 
 
-        SingleOutputStreamOperator<Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer>> reduce = keyedStream.reduce(
-                new ReduceFunction<Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer>>() {
-
-
-                    public Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer> reduce(Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer> value1,
-                                                                                                           Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer> value2) throws Exception {
-
-                        Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer> out = new Tuple3<Long, String, Double>(value2.f0, value1.f1,value1.f2+value2.f2);
-                        return out;
-                    }
-                }
-        );
+//        SingleOutputStreamOperator<Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer>> reduce = keyedStream.reduce(
+//                new ReduceFunction<Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer>>() {
+//
+//
+//                    public Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer> reduce(Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer> value1,
+//                                                                                                           Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer> value2) throws Exception {
+//
+//                        Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer> out = new Tuple3<Long, String, Double>(value2.f0, value1.f1,value1.f2+value2.f2);
+//                        return out;
+//                    }
+//                }
+//        );
 
 
 
@@ -136,52 +136,52 @@ public class exercise2 {
 
     }
 
-
-    private static class averageSpeedFunction implements WindowFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>, Tuple6<Integer, Integer, Integer, Integer, Integer, Double>, Tuple, TimeWindow> {
-        @Override
-        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> iterable, Collector<Tuple6<Integer, Integer, Integer, Integer, Integer, Double>> collector) throws Exception {
-            boolean segA = false, segB = false;
-            Tuple stream;
-            List<Integer> timestamp = new ArrayList<Integer>();
-            List<Integer> Distance = new ArrayList<Integer>();
-            List<Integer> speed = new ArrayList<Integer>();
-            int Time2 = 0, Time1 = Integer.MAX_VALUE;
-            int Distance1= Integer.MAX_VALUE, Distance2= 0;
-
-            OptionalDouble avgSpeed = OptionalDouble.of(0);
-            Double avgSpeedms =0.0;
-            Double avgSpeedFinal =0.0;
-            for (Tuple8 element : iterable) {
-                if ((int) element.f6 == 52) {
-                    segA = true;
-                } else if ((int) element.f6 == 56) {
-                    segB = true;
-                }
-
-                speed.add((int) element.f2);
-                timestamp.add((int) element.f0);
-                Distance.add((int) element.f7);
-
-                if (segA && segB) {
-                    Time2 = Collections.max(timestamp);
-                    Time1 = Collections.min(timestamp);
-
-                    Distance2 = Collections.max(Distance);
-                    Distance1 = Collections.min(Distance);
-
-                    //Double variable = Calculate the speed which velocity = distance2 - distance 1 / time2 - time1
-                    //Distnace*2.237
-
-                    avgSpeedms = (Distance2-Distance1)/(Time2-Time1)+0.0;
-                    avgSpeedFinal = avgSpeedms*2.23694;
-                    //counter +1
-                }
-                //Divide sum of velocity/count
-            }
-            if(avgSpeedFinal > 60.0)
-                collector.collect(new Tuple6<Integer, Integer, Integer, Integer, Integer, Double>(Time1, Time2, Integer.parseInt(tuple.getField(0).toString()), Integer.parseInt(tuple.getField(1).toString()), Integer.parseInt(tuple.getField(2).toString()), avgSpeedFinal));
-        }
-    }
+//
+//    private static class averageSpeedFunction implements WindowFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>, Tuple6<Integer, Integer, Integer, Integer, Integer, Double>, Tuple, TimeWindow> {
+//        @Override
+//        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> iterable, Collector<Tuple6<Integer, Integer, Integer, Integer, Integer, Double>> collector) throws Exception {
+//            boolean segA = false, segB = false;
+//            Tuple stream;
+//            List<Integer> timestamp = new ArrayList<Integer>();
+//            List<Integer> Distance = new ArrayList<Integer>();
+//            List<Integer> speed = new ArrayList<Integer>();
+//            int Time2 = 0, Time1 = Integer.MAX_VALUE;
+//            int Distance1= Integer.MAX_VALUE, Distance2= 0;
+//
+//            OptionalDouble avgSpeed = OptionalDouble.of(0);
+//            Double avgSpeedms =0.0;
+//            Double avgSpeedFinal =0.0;
+//            for (Tuple8 element : iterable) {
+//                if ((int) element.f6 == 52) {
+//                    segA = true;
+//                } else if ((int) element.f6 == 56) {
+//                    segB = true;
+//                }
+//
+//                speed.add((int) element.f2);
+//                timestamp.add((int) element.f0);
+//                Distance.add((int) element.f7);
+//
+//                if (segA && segB) {
+//                    Time2 = Collections.max(timestamp);
+//                    Time1 = Collections.min(timestamp);
+//
+//                    Distance2 = Collections.max(Distance);
+//                    Distance1 = Collections.min(Distance);
+//
+//                    //Double variable = Calculate the speed which velocity = distance2 - distance 1 / time2 - time1
+//                    //Distnace*2.237
+//
+//                    avgSpeedms = (Distance2-Distance1)/(Time2-Time1)+0.0;
+//                    avgSpeedFinal = avgSpeedms*2.23694;
+//                    //counter +1
+//                }
+//                //Divide sum of velocity/count
+//            }
+//            if(avgSpeedFinal > 60.0)
+//                collector.collect(new Tuple6<Integer, Integer, Integer, Integer, Integer, Double>(Time1, Time2, Integer.parseInt(tuple.getField(0).toString()), Integer.parseInt(tuple.getField(1).toString()), Integer.parseInt(tuple.getField(2).toString()), avgSpeedFinal));
+//        }
+//    }
 
 
     public static class SimpleSum implements WindowFunction<Tuple8<Long, Long, Integer, Integer, Integer, Integer, Integer, Integer>,
